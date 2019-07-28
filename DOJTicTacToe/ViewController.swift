3886ecdaa2977b6a0255c404099350d5f166cc59
JerryTan <jryz101@gmail.com>
2019-07-28 22:16:50 +0800

    3. Link up tappedButton action, and empty the button images.

--- DOJTicTacToe/ViewController.swift
+++ DOJTicTacToe/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func tappedButton(_ sender: UIButton) {
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
