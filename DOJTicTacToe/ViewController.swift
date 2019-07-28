b13892aad0ceb3e48ed9f5c87b44db612505ba7c
JerryTan <jryz101@gmail.com>
2019-07-28 22:33:28 +0800

    4. Set tag to identify all the tappedButton number, created activePlayer variable, and composed a conditional methods to track activePlayer role.

--- DOJTicTacToe/ViewController.swift
+++ DOJTicTacToe/ViewController.swift
@@ -1,22 +1,40 @@
-//
 //  ViewController.swift
 //  DOJTicTacToe
-//
 //  Created by Jerry Tan on 28/07/2019.
 //  Copyright © 2019 Starknet Technologies®. All rights reserved.
-//
+
 
 import UIKit
 
 class ViewController: UIViewController {
+    
+    //activePlayer variable
+    var activePlayer: Int = 1
+    
 
     @IBAction func tappedButton(_ sender: UIButton) {
+        
+        // 1 - Batman 2 - Superman
+        //Conditional methods to keep track player stats
+        if activePlayer == 1 {
+            
+            //Set the image to appear when the user tap the game board.
+            sender.setImage(UIImage(named: "Batman.png"), for: [])
+            activePlayer = 2
+            
+        }else{
+            
+            //Set the image to appear when the user tap the game board.
+            sender.setImage(UIImage(named: "Superman.png"), for: [])
+            activePlayer = 1
+        }
+        
+        print(sender.tag)
     }
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 }
 
