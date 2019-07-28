466b7efe5726531686ed631f2e8d6aae91403684
JerryTan <jryz101@gmail.com>
2019-07-29 01:12:52 +0800

    5. Created gameState array, and activePosition variable to keep track player role.

--- DOJTicTacToe/ViewController.swift
+++ DOJTicTacToe/ViewController.swift
@@ -11,11 +11,23 @@ class ViewController: UIViewController {
     //activePlayer variable
     var activePlayer: Int = 1
     
+    //gameState variable
+    var gameState = [0, 0, 0, 0, 0, 0, 0, 0, 0,]
+    
 
     @IBAction func tappedButton(_ sender: UIButton) {
         
+        //Set activePosition equals to sender.tag - 1
+        let activePosition = sender.tag - 1
+        
+        
         // 1 - Batman 2 - Superman
-        //Conditional methods to keep track player stats
+        //Conditional methods to keep track activePlayer stats
+        if gameState[activePosition] == 0 {
+            
+        gameState[activePosition] = activePlayer
+        
+        
         if activePlayer == 1 {
             
             //Set the image to appear when the user tap the game board.
@@ -28,10 +40,8 @@ class ViewController: UIViewController {
             sender.setImage(UIImage(named: "Superman.png"), for: [])
             activePlayer = 1
         }
-        
-        print(sender.tag)
     }
-    
+}
     
     override func viewDidLoad() {
         super.viewDidLoad()
