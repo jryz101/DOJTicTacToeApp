18853ab36b4aca7eb38c64d4e97b10cd5ca8323d
JerryTan <jryz101@gmail.com>
2019-07-29 02:03:06 +0800

    7. Added winnerLabel, playAgainButton outlets, and created playAgain button action.

--- DOJTicTacToe/ViewController.swift
+++ DOJTicTacToe/ViewController.swift
@@ -8,6 +8,12 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    
+    
+    @IBOutlet weak var winnerLabel: UILabel!
+    @IBOutlet weak var playAgainButton: UIButton!
+    
+    
     //activegame variable
     var activeGame = true
     
@@ -19,6 +25,14 @@ class ViewController: UIViewController {
     
     //WinningCombinations array.
     let winningCombinations = [[0, 1, 2],[3, 4, 5],[6, 7, 8],[0, 3, 6], [1, 4, 7],[2, 5, 8], [0, 4, 8],[2,4,6]]
+    
+    
+    
+    
+    
+    @IBAction func playAgain(_ sender: UIButton) {
+    }
+    
     
 
     @IBAction func tappedButton(_ sender: UIButton) {
