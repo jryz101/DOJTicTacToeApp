1bff5517959f7ae8466a32d86c2516951e634691
JerryTan <jryz101@gmail.com>
2019-07-29 01:35:05 +0800

    6. Created winningCombinations array, activeGame variable, and composed winningCombinations for in loop methods.

--- DOJTicTacToe/ViewController.swift
+++ DOJTicTacToe/ViewController.swift
@@ -8,11 +8,17 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    //activegame variable
+    var activeGame = true
+    
     //activePlayer variable
     var activePlayer: Int = 1
     
     //gameState variable
     var gameState = [0, 0, 0, 0, 0, 0, 0, 0, 0,]
+    
+    //WinningCombinations array.
+    let winningCombinations = [[0, 1, 2],[3, 4, 5],[6, 7, 8],[0, 3, 6], [1, 4, 7],[2, 5, 8], [0, 4, 8],[2,4,6]]
     
 
     @IBAction func tappedButton(_ sender: UIButton) {
@@ -23,7 +29,7 @@ class ViewController: UIViewController {
         
         // 1 - Batman 2 - Superman
         //Conditional methods to keep track activePlayer stats
-        if gameState[activePosition] == 0 {
+        if gameState[activePosition] == 0 && activeGame {
             
         gameState[activePosition] = activePlayer
         
@@ -40,6 +46,20 @@ class ViewController: UIViewController {
             sender.setImage(UIImage(named: "Superman.png"), for: [])
             activePlayer = 1
         }
+        //For in loop combination algorithm to determine winner.
+        for combination in winningCombinations {
+                
+                if gameState[combination[0]] != 0 && gameState[combination[0]] == gameState[combination[1]] && gameState[combination[1]] == gameState[combination[2]] {
+                    
+                    //Winner results
+                    activeGame = false
+                    
+                    //Writes the textual representations of the given items into the standard output.
+                    print(gameState[combination[0]])
+                    
+                    
+                }
+            }
     }
 }
     
